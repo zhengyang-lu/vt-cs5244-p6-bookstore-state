a68c578d1a89d20cd793da5ad1f83dc159d251a0
Zhengyang Lu <zhengyang.lu.x@gmail.com>
2025-10-21 20:15:55 -0400

    Remove console log messages

--- zhengyang-bookstore-state-client/src/components/CategoryBookList.tsx
+++ zhengyang-bookstore-state-client/src/components/CategoryBookList.tsx
@@ -9,8 +9,6 @@ import CategoryBookListItem from "./CategoryBookListItem.tsx";
 
 export default function CategoryBookList() {
   const { categoryName } = useParams();
-  console.log("category name inside book list component");
-  console.log(categoryName);
 
   const [bookList, setBookList] = useState([]);
 
@@ -20,8 +18,6 @@ export default function CategoryBookList() {
           .catch(console.error);
   }, [categoryName]);
   
-  console.log("book list inside book list component");
-  console.log(bookList);
 
   const bookBoxList = bookList.map((myBook) => (
     <li>
--- zhengyang-bookstore-state-client/src/data.ts
+++ zhengyang-bookstore-state-client/src/data.ts
@@ -6,23 +6,17 @@ export function getCategories(): CategoryItem[] {
 }
 
 export function getBooksForCategory(categoryName: string): BookItem[] {
-  console.log("category name inside data.ts");
-  console.log(categoryName);
+
   const selectedCategory: CategoryItem = sourceData.categories.find(
     (category: CategoryItem) => category.name === categoryName
   ) as CategoryItem;
-  console.log("selected category inside data.ts in get books for category");
-  console.log(selectedCategory);
-  console.log(selectedCategory.categoryId);
+
   const fullBookList: BookItem[] = sourceData.books;
-  console.log(fullBookList);
+
 
   const filteredBookList = fullBookList.filter(
     (book) => book.categoryId === selectedCategory.categoryId
   );
-  console.log(
-    "booklist inside data.ts in get books for category, before returning"
-  );
-  console.log(filteredBookList);
+
   return filteredBookList;
 }
