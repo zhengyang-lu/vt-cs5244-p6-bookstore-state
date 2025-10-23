bcea3b6445b1e12c63f839549fc2c3be147660af
Zhengyang Lu <zhengyang.lu.x@gmail.com>
2025-10-22 20:24:39 -0400

    Fix npm run build issues

--- zhengyang-bookstore-state-client/src/pages/CategoryPage.tsx
+++ zhengyang-bookstore-state-client/src/pages/CategoryPage.tsx
@@ -1,13 +1,12 @@
 import CategoryNav from "../components/CategoryNav";
 // import child components
 import CategoryBookList from "../components/CategoryBookList";
-import { CategoryProps } from "../types";
 // import CSS style file (note: no styles for this page)
 // export component function
-export default function CategoryPage(props: CategoryProps) {
+export default function CategoryPage() {
   return (
     <div className="category-page">
-      <CategoryNav categories={props.categories}/>
+      <CategoryNav />
       <CategoryBookList />
     </div>
   );
